4073bb624c5f6c3ceb960427172946dd37760487
Nguyen Van Dat <datsunbae@gmail.com>
2022-09-15 17:18:44 +0700

    Render list photo

--- src/features/Photo/pages/Main/index.jsx
+++ src/features/Photo/pages/Main/index.jsx
@@ -6,10 +6,19 @@ import Images from 'constants/images';
 import './Main.scss';
 import { useSelector } from 'react-redux';
 import { photoReducer } from 'app/store';
+import PhotoList from 'features/Photo/components/PhotoList';
 
 
 function Main(props) {
   const listPhoto = useSelector(state => state.photos);
+
+  const handleEditPhoto = (photo) => {
+    console.log('Edit', photo);
+  }
+
+  const handleRemovePhoto = (photo) => {
+    console.log('Remove', photo);
+  }
   return (
     <div className='photo-main'>
       <Banner
@@ -19,6 +28,11 @@ function Main(props) {
 
       <Container className='text-center'>
         <Link to='/photos/add' className='text-decoration-none photo-main__add-photo'>Add new photo</Link>
+        <PhotoList
+          photoList={listPhoto}
+          onPhotoEditClick={handleEditPhoto}
+          onPhotoRemoveClick={handleRemovePhoto}
+        />
       </Container>
     </div>
   )
